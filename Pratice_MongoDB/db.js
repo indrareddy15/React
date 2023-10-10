4d4876342cfdfeb6080540266c1418c64dc4de89
indrareddy15 <gantaindrareddy83@gmail.com>
2023-10-10 23:00:14 +0530

    CURD Opreations in MongoDB

--- Pratice_MongoDB/db.js
+++ Pratice_MongoDB/db.js
@@ -1,15 +1,20 @@
 const { MongoClient } = require("mongodb");
 
 let dbConnection;
-const conntectDB = () => {
+const conntectDB = (callBackFun) => {
     MongoClient.connect('mongodb://localhost:27017/booksstore')
         .then((client) => {
             dbConnection = client.db();
+            return callBackFun()
+        })
+        .catch((err) => {
+            console.log(err);
+            return callBackFun();
         })
 }
 
 const getDB = () => {
-
+    return dbConnection
 }
 
 module.exports = {
