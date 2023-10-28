08a96033a74eab68f072e1ffb4caf3918f4aa304
indrareddy15 <gantaindrareddy83@gmail.com>
2023-10-28 22:12:34 +0530

    Goals commit

--- MERN/backend/middleware/errorMiddleware.js
+++ MERN/backend/middleware/errorMiddleware.js
@@ -7,7 +7,7 @@ const errorHandler = (err, req, res, next) => {
         message: err.message,
 
         // stack trace will some aditional information about if that is production mode only
-        stack: process.env.NODE_ENV === 'production' ? nullll : err.stack,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
     })
 }
 
--- MERN/backend/routes/goalsRoutes.js
+++ MERN/backend/routes/goalsRoutes.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-const { getGoals, postGoals, putGoals, deleteGoals } = require('../controllers/goalControllers');
+const { getGoals, postGoals, putGoals, deleteGoals } = require('./goalsRoutes');
+// import { getGoals, postGoals, putGoals, deleteGoals } from '../../controllers/goalControllers';
 
-
-router.route('/').get(getGoals).post(postGoals)
-
+// router.route('/').get(getGoals).post(postGoals)
 // Since get and post are having same routes they can be chained into one by using routes
 
-// router.get('/', getGoals)
-// router.post('/', postGoals)
+router.get('/', getGoals)
 
-router.route('/:id').put(putGoals).delete(deleteGoals);
+router.post('/', postGoals)
 
 // Similirly put and delete are having same routes they can be chained into one by using routes
+// router.route('/:id').put(putGoals).delete(deleteGoals);
 
-// router.put('/:id', putGoals)
-// router.delete('/:id', deleteGoals)
+
+router.put('/:id', putGoals)
+
+router.delete('/:id', deleteGoals)
 
 module.exports = router
