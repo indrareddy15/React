7c2ce9ad2b3ee7f03538395a29277a38f8c992ce
indrareddy15 <gantaindrareddy83@gmail.com>
2024-06-21 19:14:14 +0530

    BinarySearch

--- CRIO/class/src/Components/BigO/BinarSearch.js
+++ CRIO/class/src/Components/BigO/BinarSearch.js
@@ -129,3 +129,28 @@ function stringCompres(n, str) {
 
 let strCompres = stringCompres("1001010");
 console.log(strCompres);
+
+
+function search(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (nums[left] === target) {
+      return mid;
+    } else if (nums[left] <= mid) {
+      if (nums[left] <= target && target <= nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } else {
+      if (target > nums[mid] && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+  return -1;
+}
