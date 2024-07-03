d90b870f81e3613c25c8f13ac621515a082dce37
indrareddy15 <gantaindrareddy83@gmail.com>
2024-07-03 11:47:00 +0530

    basic commit

--- lic/src/App.js
+++ lic/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
+import CustomerManagement from "./Component/License";
 // import LicenseManager from "./Component/License";
-import LicenseManager from "./Component/LicenseManager";
+// import LicenseManager from "./Component/LicenseManager";
 
 function App() {
   return (
     <div className="App">
-      <LicenseManager />
+      {/* <LicenseManager /> */}
+      <CustomerManagement />
     </div>
   );
 }
