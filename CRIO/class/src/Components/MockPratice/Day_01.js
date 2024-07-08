c328c166f486cb004d58f2ece5dde2161d7181c1
indrareddy15 <gantaindrareddy83@gmail.com>
2024-07-08 10:51:08 +0530

    BinarySearch

--- CRIO/class/src/Components/MockPratice/Day_01.js
+++ CRIO/class/src/Components/MockPratice/Day_01.js
@@ -62,7 +62,7 @@ function countVowels(word) {
       count++;
     }
   }
-  return vw;
+  return count;
 }
 
 // console.log(countVowels("Indra Reddy Ganta"));
@@ -103,8 +103,8 @@ function magicSquares(m, matrix) {
   let sumD1 = 0;
   let sumD2 = 0;
   for (let i = 0; i < m; i++) {
-      sumD1 += matrix[i][i];
-      sumD2 += matrix[i][m - 1 - i];
+    sumD1 += matrix[i][i];
+    sumD2 += matrix[i][m - 1 - i];
   }
 
   if (sumD1 !== sumD2) {
@@ -132,3 +132,50 @@ let magic = magicSquares(3, [
   [8, 1, 6],
 ]);
 console.log(magic);
+
+function findTarget(n, arr, tar) {
+  let l = 0;
+  let r = n - 1;
+  let mid;
+  while (l <= r) {
+    mid = Math.floor((l + r) / 2);
+    if (arr[mid] === tar) {
+      return mid;
+    } else if (arr[mid] > tar) {
+      r = mid - 1;
+    } else {
+      l = mid + 1;
+    }
+  }
+  return -1;
+}
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const toFind = 7;
+const n = arr.length;
+console.log("findTarget", findTarget(n, arr, toFind));
+
+function searchrarget(matrix, tar) {
+  let row = 0;
+  let col = matrix[0].length - 1;
+
+  while (row < matrix.length && col >= 0) {
+    if (matrix[row][col] === tar) {
+      return true;
+    } else if (matrix[row][col] > tar) {
+      col--;
+    } else {
+      row++;
+    }
+  }
+  return -1;
+}
+
+function reverseWordsInAString(s) {
+    let str = s.split(" ");
+    return str.reverse().join(" ").trim();
+}
+
+// Example usage
+const inputString = "  hello world  ";
+console.log(reverseWordsInAString(inputString)); // Output: "world hello"
