0c7600b5477eaf9dd4a39ff8605103dcf908294a
indrareddy15 <gantaindrareddy83@gmail.com>
2024-07-13 12:02:59 +0530

    Mock

--- CRIO/class/src/Components/MockPratice/Day_01.js
+++ CRIO/class/src/Components/MockPratice/Day_01.js
@@ -281,3 +281,18 @@ function coPrime(n) {
   return cnt;
 }
 console.log("CoPrime", coPrime(9));
+
+function palindorme(str) {
+  let left = 0;
+  let right = str.length - 1;
+
+  while (left < right) {
+    if (str[left] !== str[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+}
+console.log("palindorme", palindorme("madams"));
