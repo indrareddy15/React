5b124c26088bfdfd5fa552db6f746b468aa95b39
indrareddy15 <gantaindrareddy83@gmail.com>
2024-07-15 12:22:33 +0530

    peak_ele

--- CRIO/class/src/Components/MockPratice/Day_01.js
+++ CRIO/class/src/Components/MockPratice/Day_01.js
@@ -296,3 +296,64 @@ function palindorme(str) {
   return true;
 }
 console.log("palindorme", palindorme("madams"));
+
+function bestTime(prices) {
+  let left = 0;
+  let right = prices.length - 1;
+  while (left < right) {
+    let maxPrice = prices[right] - prices[left];
+    if (maxPrice === Math.max(prices)) {
+      return maxPrice;
+    }
+    left++;
+    right--;
+  }
+  return 0;
+}
+
+function findPeakElement(arr) {
+  let n = arr.length;
+  if (n === 1) return 0;
+  if (arr[0] >= arr[1]) return 0;
+  if (arr[n - 1] >= arr[n - 2]) return n - 1;
+
+  for (let i = 1; i < n - 1; i++) {
+    if (arr[i] >= arr[i - 1] && arr[i] >= arr[i + 1]) return i;
+  }
+}
+console.log("findPeakElement1", findPeakElement([10, 20, 15, 2, 23, 90, 67]));
+console.log("findPeakElement2", findPeakElement([1, 3, 20, 4, 1, 0]));
+
+// A JavaScript program to find a peak element
+// using divide and conquer
+
+// A binary search based function
+// that returns index of a peak element
+function findPeakUtil(arr, n) {
+  let l = 0;
+  let r = n - 1;
+  let mid;
+
+  while (l <= r) {
+    // finding the mid index by right shifting
+    mid = (l + r) >> 1;
+
+    // first case if mid is the answer
+    if (
+      (mid === 0 || arr[mid - 1] <= arr[mid]) &&
+      (mid === n - 1 || arr[mid + 1] <= arr[mid])
+    )
+      break;
+
+    // change the right pointer to mid-1
+    if (mid > 0 && arr[mid - 1] > arr[mid]) r = mid - 1;
+    // change the left pointer to mid+1
+    else l = mid + 1;
+  }
+
+  return mid;
+}
+
+// A wrapper over recursive function findPeakUtil()
+let peak = findPeakUtil([1, 3, 20, 4, 1, 0], arr.length);
+console.log("Index of a peak point is " + peak);
