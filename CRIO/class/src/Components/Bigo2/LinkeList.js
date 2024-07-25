6a3d5cbe923d9a76adff835c2c867e4b15db5044
indrareddy15 <gantaindrareddy83@gmail.com>
2024-07-25 14:43:31 +0530

    LinkedList

--- CRIO/class/src/Components/Bigo2/LinkeList.js
+++ CRIO/class/src/Components/Bigo2/LinkeList.js
@@ -15,7 +15,7 @@ class LinkedList {
     return this.size === 0;
   }
 
-  isSize() {
+  getSize() {
     return this.size;
   }
 
@@ -29,17 +29,41 @@ class LinkedList {
       this.head = newNode;
     }
     this.size++;
-    }
-    
-    addEleAtEndNode(value) {
-        let newNode = new Node(value)
+  }
 
-        if (this.head === null) {
-            this.head = newNode
-        } else {
-            
-        }
+  addEleAtEndNode(value) {
+    let newNode = new Node(value);
+
+    if (this.head === null()) {
+      this.head = newNode;
+    } else {
+      let prev = this.head;
+      while (prev.next !== null) {
+        prev = prev.next;
+      }
+      prev.next = newNode;
     }
+    this.size++;
+  }
+
+  addNodeAtIndexLevel(value, index) {
+    if (index < 0 || index > this.size) {
+      return;
+    }
+
+    if (index === 0) {
+      this.addEleAtStartNode(value);
+    } else {
+      let newNode = new Node(value);
+      let prev = this.head;
+      for (let i = 0; i < index - 1; i++) {
+        prev = prev.next;
+      }
+      newNode.next = prev.next;
+      prev.next = newNode;
+      this.size++;
+    }
+  }
 
   printLinkedList() {
     let curr = this.head;
@@ -54,11 +78,22 @@ class LinkedList {
 
 let l1 = new LinkedList();
 console.log("Before List Creation", l1.isEmpty());
-console.log("Before List Creation Size", l1.isSize());
-l1.addEleAtStartNode(2);
-l1.addEleAtStartNode(4);
-l1.addEleAtStartNode(6);
+console.log("Before List Creation Size", l1.getSize());
+// Start of end
 l1.addEleAtStartNode(8);
+l1.addEleAtStartNode(6);
+l1.addEleAtStartNode(4);
+l1.addEleAtStartNode(2);
 l1.printLinkedList();
 console.log("After List Creation", l1.isEmpty());
-console.log("After List Creation Size", l1.isSize());
+console.log("After List Creation Size", l1.getSize());
+// End of node
+l1.addEleAtEndNode(11);
+l1.addEleAtEndNode(13);
+l1.printLinkedList();
+
+// Inserting at index level
+l1.addNodeAtIndexLevel(3, 1);
+l1.addNodeAtIndexLevel(5, 3);
+l1.addNodeAtIndexLevel(1, 0);
+l1.printLinkedList();
