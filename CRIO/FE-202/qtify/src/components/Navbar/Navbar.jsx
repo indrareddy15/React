787898a7c3b093e7921616a0096a0b057355130d
indrareddy15 <gantaindrareddy83@gmail.com>
2024-09-11 21:25:25 +0530

    Navbar

--- CRIO/FE-202/qtify/src/components/Navbar/Navbar.jsx
+++ CRIO/FE-202/qtify/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Button from "../Button/Button";
+// import Button from "../Button/Button";
 import Logo from "../Logo/Logo";
 import Search from "../Search/Search";
 import styles from "./Navbar.module.css";
@@ -15,7 +15,7 @@ function Navbar({ searchData }) {
         placeholder="Search a song of your choice"
         searchData={searchData}
       />
-      <Button>Give Feedback</Button>
+      {/* <Button>Give Feedback</Button> */}
     </nav>
   );
 }
