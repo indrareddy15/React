5844d82ab2fad316d36ea762c18cee05ea9b31e9
indrareddy15 <gantaindrareddy83@gmail.com>
2024-09-19 10:58:30 +0530

    Delete_Fix

--- xstopwatch-/src/utils/localStorage.jsx
+++
@@ -1,8 +0,0 @@
-export const saveToLocalStorage = (key, value) => {
-  localStorage.setItem(key, JSON.stringify(value));
-};
-
-export const loadFromLocalStorage = (key) => {
-  const item = localStorage.getItem(key);
-  return item ? JSON.parse(item) : null;
-};
