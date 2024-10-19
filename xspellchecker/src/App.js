949ef6d5ab6f2c30d958715d92822d6b543a39e1
indrareddy15 <gantaindrareddy83@gmail.com>
2024-10-19 15:28:10 +0530

    spellCHecker

---
+++ xspellchecker/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import XSpellChecker from './components/XSpellChecker';
+
+function App() {
+  return (
+    <div className="App">
+      <XSpellChecker />
+    </div>
+  );
+}
+
+export default App;
