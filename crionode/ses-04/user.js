b65448d8c981dd1351f9d99cd074b1b76ec4be56
indrareddy15 <gantaindrareddy83@gmail.com>
2024-11-13 23:06:43 +0530

    user commit

---
+++ crionode/ses-04/user.js
@@ -0,0 +1,12 @@
+// import express from "express";
+// import userRouter from "./routes/user.routes.js";
+
+// const app = express();
+
+// const port = 8086;
+
+// app.use("/users", userRouter)
+
+// app.listen(port, () => {
+//     console.log(`Server is running on http://localhost:${port}`);
+// });
