d2a80778c3bdd792c20ca7e5d5225279cc7aca95
indrareddy15 <gantaindrareddy83@gmail.com>
2024-11-16 18:25:27 +0530

    sses-04 Server

--- crionode/session-04/index.js
+++ crionode/session-04/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const { router: userRouter } = require('./routes/user.routes.js');
-
 const { router: todoRouter } = require("./routes/todo.routes.js")
+
+const { verifyAuth } = require('./middleware/verifyAuth.js');
+
+const dotenv = require("dotenv");
 
 const app = express();
 const port = 8086;
 
+dotenv.config()
+
+app.use(verifyAuth)
 
 app.use("/users", userRouter);
 app.use("/todos", todoRouter)
