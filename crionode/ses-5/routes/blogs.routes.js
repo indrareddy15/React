d629fef597b66e829f10c60ca4798244694fc9bf
indrareddy15 <gantaindrareddy83@gmail.com>
2024-11-20 21:57:07 +0530

    blog routes

--- crionode/ses-5/routes/blogs.routes.js
+++ crionode/ses-5/routes/blogs.routes.js
@@ -1,10 +1,28 @@
 const Router = require('express');
-const { createBlog, getBlogs, getBlogById } = require('../controller/blogs.controller');
+const { createBlog, getBlogs, getBlogById, updateBlogById, deleteBlogById } = require('../controller/blogs.controller');
+const findBlogWithId = require('../middlewares/findBlogById');
 
 const router = Router();
 
 router.post("/new", createBlog);
 router.get("/", getBlogs);
-router.get("/:blogId", getBlogById);
+
+router
+    .route("/:blogId")
+    .all(findBlogWithId) // .all method will run all common routes for the blogId route
+    .get(getBlogById)
+    .patch(updateBlogById)
+    .delete(deleteBlogById)
+
+
+// router
+//     .route("/:blogId")
+//     .get(findBlogWithId, getBlogById)
+//     .patch(findBlogWithId, updateBlogById)
+//     .delete(findBlogWithId, deleteBlogById)
+
+// router.get("/:blogId", getBlogById);
+// router.patch("/:blogId", updateBlogById)
+// router.delete("/:blogId", deleteBlogById)
 
 module.exports = router;
