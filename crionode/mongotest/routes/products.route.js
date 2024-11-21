19cc8f6ec83a39095a5189cf626dd7e785db044a
indrareddy15 <gantaindrareddy83@gmail.com>
2024-11-21 22:34:02 +0530

    route fix

--- crionode/mongotest/routes/products.route.js
+++ crionode/mongotest/routes/products.route.js
@@ -1,5 +1,6 @@
 const Router = require('express');
 const { createProduct, getProductById, getAllProducts, updateProductById, deleteProductById, } = require('../controllers/products.controller');
+const { findProductById } = require('../middlewar/findbyId');
 
 const router = Router();
 
@@ -8,7 +9,12 @@ router.post("/new", createProduct);
 
 router.get("/", getAllProducts);
 
-router.route("/:id").get(getProductById).patch(updateProductById).delete(deleteProductById);
+router
+    .route("/:id")
+    .all(findProductById)
+    .get(getProductById)
+    .patch(updateProductById)
+    .delete(deleteProductById);
 
 // router.get("/:id", getProductById)
 // router.patch("/:id", updateProductById)
