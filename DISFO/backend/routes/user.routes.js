d2864e7b525013dd8e8d482fabec679e41380f4b
indrareddy15 <gantaindrareddy83@gmail.com>
2024-12-06 22:26:10 +0530

    userRoute

--- DISFO/backend/routes/user.routes.js
+++ DISFO/backend/routes/user.routes.js
@@ -7,11 +7,12 @@ const {
 const { checkAdminKey } = require("../middlewares/admin.middleware");
 const { userValidationSchema } = require("../validations/user.validator");
 const { validateSchema } = require("../middlewares/validate.middleware");
+const auhtorize = require("../middlewares/authorize-jwt.middleware")
 
 const validateUser = validateSchema(userValidationSchema);
 
 router.post("/register", validateUser, postRegister);
 router.get("/all", checkAdminKey, getAllUsers);
-router.get("/:username", getUserByUsername);
+router.get("/:username", auhtorize, getUserByUsername);
 
 module.exports = router;
