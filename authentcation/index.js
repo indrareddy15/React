b9116156a1bd2696db13884b4f639b9fe49b34c7
indrareddy15 <gantaindrareddy83@gmail.com>
2024-12-08 21:48:32 +0530

    server

---
+++ authentcation/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./db/db.connection");
+const userRoute = require("./routes/user.route")
+
+dotenv.config()
+
+connectDB()
+const app = express();
+const port = process.env.PORT || 3001
+
+app.use(express.json())
+app.use("/user", userRoute)
+
+
+app.listen(port, () => {
+    console.log(`Server is running at ${port}`)
+})
