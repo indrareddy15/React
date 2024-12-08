2d8388f98f6b4f6069cd87fdd6aa42648ad43ee0
indrareddy15 <gantaindrareddy83@gmail.com>
2024-12-08 21:48:01 +0530

    user model

---
+++ authentcation/model/user.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        default: ""
+    },
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        unique: true
+    }
+})
+
+const userModel = mongoose.model("users", userSchema);
+
+module.exports = userModel
