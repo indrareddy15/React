052be57afff3525822680a4b5a6259c1554272ad
indrareddy15 <gantaindrareddy83@gmail.com>
2024-12-09 22:52:10 +0530

    server fix

--- authentcation/index.js
+++ authentcation/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./db/db.connection");
 const userRoute = require("./routes/user.route")
+const authRoute = require("./routes/auth.route")
 
 dotenv.config()
 
@@ -10,6 +11,8 @@ const app = express();
 const port = process.env.PORT || 3001
 
 app.use(express.json())
+
+app.use("/auth", authRoute)
 app.use("/user", userRoute)
 
 
