41ca53441f5d1228031ca3516ce7438a43cd441e
indrareddy15 <gantaindrareddy83@gmail.com>
2024-12-09 22:51:12 +0530

    userRoute

--- authentcation/routes/user.route.js
+++ authentcation/routes/user.route.js
@@ -1,9 +1,13 @@
 const Router = require("express");
 const { postUser, getAllUser, getUserByName } = require("../controller/user.controller");
+const { validateSchema } = require("../middleware/user.middleware")
+const { userValidationSchema } = require("../validations/user.validation")
 
 const router = Router();
 
-router.post("/post", postUser);
+const validateUser = validateSchema(userValidationSchema)
+
+router.post("/post", validateUser, postUser);
 router.get("/all", getAllUser);
 router.get("/:username", getUserByName);
 
