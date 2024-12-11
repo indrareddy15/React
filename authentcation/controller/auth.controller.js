0ca43be5b8eafa4ae613110af16e0c10b50db873
indrareddy15 <gantaindrareddy83@gmail.com>
2024-12-11 21:03:08 +0530

    auth controller

--- authentcation/controller/auth.controller.js
+++ authentcation/controller/auth.controller.js
@@ -15,19 +15,24 @@ const postSignup = async (req, res) => {
 }
 
 const postLogin = async (req, res) => {
-    try {
-        const { username, password } = req.body;
-        const user = await userServiceInstance.getUserByName(username);
+    const { username, password } = req.body;
 
-        if (!user) { // Check only the user if you check the password while comaing the password with hashed password you may get error
-            return res.status(401).json({ message: "Login failed", reason: "User not found" });
+    if (!username || !password) {
+        return res.status(400).json({ message: "Login failed", reason: "Username and password are required" });
+    }
+
+    try {
+        const user = await userServiceInstance.getUserByName(username);
+        if (!user) {
+            return res.status(404).json({ message: `User with username ${username} could not be found!` });
         }
 
         const matchedPwd = await authServiceInstance.comparePassword(password, user.password);
-        console.log("matchedPwd", matchedPwd);
-        console.log("password", password);
-        console.log("user.password", user.password);
+
         if (matchedPwd) {
+            const token = authServiceInstance.generateJwt({ userId: user._id });
+            console.log("token", token);
+            res.cookie("token", token, { httpOnly: true, maxAge: 2 * 60 * 1000 });
             res.status(200).json({ message: "Login successful", user });
         } else {
             res.status(401).json({ message: "Login failed", reason: "Invalid credentials" });
