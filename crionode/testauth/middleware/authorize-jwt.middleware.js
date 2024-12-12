b8a58b0c9f0cf4e7ae15220b008ab02a28b4c974
indrareddy15 <gantaindrareddy83@gmail.com>
2024-12-12 21:06:10 +0530

    authorizejswt

---
+++ crionode/testauth/middleware/authorize-jwt.middleware.js
@@ -0,0 +1,23 @@
+const UserService = require('../services/user.service');
+const AuthService = require('../services/auth.service');
+
+const userService = new UserService();
+const authService = new AuthService();
+
+const authorize = async (req, res, next) => {
+    try {
+        const authToken = req.headers.authorization;
+        console.log(authToken)
+        const { userId } = await authService.verfityJwt(authToken);
+        console.log(userId)
+        const user = await userService.findById(userId);
+        console.log(user)
+        req.user = user;
+        next();
+    } catch (error) {
+        console.log(error)
+        res.status(401).send({ message: 'Unauthorized' });
+    }
+}
+
+module.exports = authorize;
