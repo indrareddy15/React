e9e4c5b70b5881e9cc0e4e40264c3cd2ba0d028d
indrareddy15 <gantaindrareddy83@gmail.com>
2024-12-12 21:03:39 +0530

    authserivce

---
+++ crionode/testauth/services/auth.service.js
@@ -0,0 +1,39 @@
+const bcrypt = require('bcrypt');
+const Jwt = require("jsonwebtoken")
+
+const UserModel = require('../model/user.model');
+
+class AuthService {
+    register = async (user) => {
+        try {
+            const hashpass = await this.generateHashedPassword(user.password)
+            user.password = hashpass
+            const newUser = await UserModel.create(user);
+            if (!newUser) {
+                throw new Error('Failed to create user');
+            }
+            return newUser;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    generateHashedPassword = async (password) => {
+        try {
+            const saltRounds = 10;
+            const hashedPassword = await bcrypt.hash(password, saltRounds);
+            return hashedPassword;
+        } catch (error) {
+            throw new Error(`Error hashing password: ${error.message}`);
+        }
+    }
+
+    comaprePassword = (password, hashedPwd) => bcrypt.compare(password, hashedPwd);
+
+    generateJwt = async (payload) => Jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    verfityJwt = async (token) => Jwt.verify(token, process.env.JWT_SECRET);
+
+}
+
+module.exports = AuthService;
