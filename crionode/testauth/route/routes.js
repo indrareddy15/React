f8c0d64bd1c129fab0729f6c44a44857285b415a
indrareddy15 <gantaindrareddy83@gmail.com>
2024-12-13 18:43:50 +0530

    routes

--- crionode/testauth/route/routes.js
+++ crionode/testauth/route/routes.js
@@ -3,6 +3,10 @@ const { createUser, getAllUsers, findUserByName } = require("../controller/user.
 const { authorization } = require("../middleware/simpelauth");
 const { userValidation } = require("../validations/user.validation");
 const { validateMiddleware } = require("../middleware/validate.middleware");
+// const authorize = require("../middleware/authorize-jwt.middleware");
+
+const authorize = require("../middleware/authorize.passport");
+
 
 const router = Router()
 
@@ -10,6 +14,6 @@ const validateUser = validateMiddleware(userValidation)
 
 router.post("/create", validateUser, createUser)
 router.get("/all", authorization, getAllUsers)
-router.get("/:username", findUserByName)
+router.get("/:username", authorize, findUserByName)
 
 module.exports = router;
