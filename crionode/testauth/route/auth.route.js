410af9b0e79a24720b945b6fa1cf099895de8b33
indrareddy15 <gantaindrareddy83@gmail.com>
2024-12-13 18:44:02 +0530

    authRoutes

--- crionode/testauth/route/auth.route.js
+++ crionode/testauth/route/auth.route.js
@@ -1,12 +1,11 @@
 const Router = require("express");
 const { createUser, loginUser } = require("../controller/auth.controller");
-const authorize = require("../middleware/authorize-jwt.middleware");
 
 
 const router = Router()
 
 router.post("/signup", createUser);
-router.post("/login", authorize, loginUser)
+router.post("/login", loginUser)
 
 
 module.exports = router;
