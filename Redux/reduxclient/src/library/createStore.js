0763c21d6690e160305e8d542421fd8aceec6234
indrareddy15 <gantaindrareddy83@gmail.com>
2025-02-12 21:47:41 +0530

    createStore

---
+++ Redux/reduxclient/src/library/createStore.js
@@ -0,0 +1,32 @@
+function createStore(initialState, handler) {
+    // Setp 1: Initialize the state
+    const state = initialState;
+    const subscribes = [];
+    // Step 2: read the state
+    // We apply abstraction here to hide the state from the outside world 
+    // // and only expose the state through the (getter function )getState function 
+    function getState() {
+        return state;
+    }
+    // Step 3: Update the state
+    function dispatch() {
+        handler(state); // This is the function that will be called when we want to update the state
+        subscribes.forEach(cb => cb()) // This will call the render function 
+    }
+
+    // Step 4: Re-render with the updated state (subscribe)
+
+    function subscribe(cb) {
+        // We will call the render function here
+        subscribes.push(cb)
+    }
+
+
+    return {
+        getState,
+        dispatch,
+        subscribe
+    }
+}
+
+export default createStore;
