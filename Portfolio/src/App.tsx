05cea5b835a71831ae921d3cdc98a9b45a1a4271
indrareddy15 <gantaindrareddy83@gmail.com>
2025-02-20 23:20:01 +0530

    subParent

---
+++ Portfolio/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navigation from './components/Navigation';
+import Hero from './components/Hero';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-background text-foreground">
+      <Navigation />
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
